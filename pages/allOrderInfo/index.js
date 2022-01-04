f69141d2b56e4994c6b3cf0e49761ad4c809bf0d
高俊杰 <gaojunjie@sunline.cn>
2022-01-04 16:55:30 +0800

    order bug fix

--- pages/allOrderInfo/index.js
+++ pages/allOrderInfo/index.js
@@ -24,7 +24,13 @@ Page({
       },
     ],
     activeTab: 0,
-    orderList: []
+    orderList: [],
+    pageParam: {
+      page: 0,
+      size: 20
+    },
+    onBottom: false,
+
   },
 
   onLoad() {
@@ -42,37 +48,47 @@ Page({
       data: {
         method: 'GET',
         param: {
-          orderStatus: status
+          orderStatus: status,
+          ...this.data.pageParam
         }
       },
     }).then(res => {
       console.log(res)
       wx.hideLoading()
-      const arr = res.result.data.map(item => {
+      const arr = this.data.orderList.concat(
+        res.result.data
+      ).map(item => {
         item.create = utils.formatDate(new Date(item.create), "yyyy-MM-dd hh:mm")
         return item
       })
 
+
       this.setData({
-        orderList: arr
+        orderList: arr,
+        onBottom: res.result.data.length === 0 || false
       })
     })
   },
 
-  onTabClick(e) {
-    const index = e.detail.index
-    // this.getOrderInfo(this.data.tabs[index].status)
-    this.setData({
-      activeTab: index
-    })
+  // onTabClick(e) {
+  //   const index = e.detail.index
+  //   // this.getOrderInfo(this.data.tabs[index].status)
+  //   this.setData({
+  //     activeTab: index
+  //   })
 
-  },
+  // },
 
   onChange(e) {
     const index = e.detail.index
     this.setData({
       activeTab: index,
-      orderList: []
+      orderList: [],
+      onBottom: false,
+      pageParam: {
+        page: 0,
+        size: 20
+      },
     })
     this.getOrderInfo(this.data.tabs[index].status)
   },
@@ -99,5 +115,14 @@ Page({
       this.getOrderInfo(this.data.tabs[this.data.activeTab].status)
     })
 
-  }
+  },
+  handleLower() {
+    const pageParam = this.data.pageParam;
+    pageParam.page++
+    console.log(pageParam)
+    this.setData({
+      pageParam
+    })
+    this.getOrderInfo(this.data.tabs[this.data.activeTab].status)
+  },
 })
